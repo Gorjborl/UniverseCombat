ea5db37eb5e0fe6e015d563e7ce0c1723627b6b1
Gorjborl <Gorjborl@gmail.com>
2018-04-29 01:07:27 -0300

    Shoot
    Shoot Particles 1

--- Assets/Scripts/BasicControls.cs
+++ Assets/Scripts/BasicControls.cs
@@ -7,9 +7,7 @@ public class BasicControls : MonoBehaviour {
 
     
     public GameObject Ship;
-    public GameObject FPCamera;
-    public GameObject ThirdCamera;
-    public int HorizontalSpeed = 0;
+    private int HorizontalSpeed = 35;
 	// Use this for initialization
 	void Start () {
 
@@ -25,31 +23,77 @@ public class BasicControls : MonoBehaviour {
 
     void UserInput()
     {
-        //Keyboard Input for Test Purposes
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += new Vector3(-0.5f, 0, 0);
-        }
+       
+            //Keyboard Input for Test Purposes
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += new Vector3(-1 * Time.deltaTime * HorizontalSpeed, 0, 0);
+                CheckValidPosition();
+            }
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(0.5f, 0, 0);
-        }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(1 * Time.deltaTime * HorizontalSpeed, 0, 0);
+                CheckValidPosition();
+            }
 
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.position += new Vector3(0, 0, 0.5f);
-        }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position += new Vector3(0, 0, 1 * Time.deltaTime * HorizontalSpeed);
+                CheckValidPosition();
+            }
 
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position += new Vector3(0, 0, -0.5f);
-        }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position += new Vector3(0, 0, -1 * Time.deltaTime * HorizontalSpeed);
+                CheckValidPosition();
+            }
 
-        if (Input.GetKey(KeyCode.Z))
-        {
-            //esta wea deberia disparar
-        }
+            if (Input.GetKey(KeyCode.Z))
+            {
+                //esta wea deberia disparar
+            }
+        
+        
     }
     
+
+    void CheckValidPosition()
+    {
+        if ( transform.position.x >= -19)
+        {
+
+        } else
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+
+
+        if(transform.position.x <= 19)
+        {
+
+        } else
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+
+        if (transform.position.z >= -7)
+        {
+
+        }
+        else
+        {
+            transform.position += new Vector3(0, 0, 1);
+        }
+
+
+        if (transform.position.z <= 53)
+        {
+
+        }
+        else
+        {
+            transform.position += new Vector3(0, 0, -1);
+        }
+    }
 }
