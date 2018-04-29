aaca4eadbefaf7272bb111a8a412e9ea06b0abee
Gorjborl <Gorjborl@gmail.com>
2018-04-28 21:36:41 -0300

    Initial commit

---
+++ Assets/Scripts/BasicControls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicControls : MonoBehaviour {
+
+
+    
+    public GameObject Ship;
+    public GameObject FPCamera;
+    public GameObject ThirdCamera;
+    public int HorizontalSpeed = 0;
+	// Use this for initialization
+	void Start () {
+
+        Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+            //Ship.transform.Translate(Input.acceleration.x * Time.deltaTime * HorizontalSpeed, 0, 0 );
+            Ship.transform.Rotate(0,-Input.gyro.rotationRateUnbiased.y, 0);
+            Ship.transform.Rotate(-Input.gyro.rotationRateUnbiased.x, 0, 0);
+            ChangeCamera();		
+	}
+
+    void ChangeCamera()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ThirdCamera.gameObject.SetActive(true);
+            FPCamera.gameObject.SetActive(false);
+            
+        }
+    }
+}
